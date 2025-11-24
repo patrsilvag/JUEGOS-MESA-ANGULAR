b6cf21c573d3d52060e09d9e2008ab68a985e98d
patrsilvag <patr.silvag@duocuc.cl>
2025-11-24 16:10:47 -0300

    Se habilitó HttpClient app.Config.ts y se creo environment.ts con apiUrl”

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,14 @@ import { ApplicationConfig, provideZoneChangeDetection, ErrorHandler } from '@an
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { GlobalErrorHandler } from './core/global-error-handler';
+import { provideHttpClient } from '@angular/common/http'; 
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-
+    // ✅ habilita HttpClient en toda la app
+    provideHttpClient(),
     // ⬇️ Registro del manejador global de errores
     { provide: ErrorHandler, useClass: GlobalErrorHandler },
   ],
